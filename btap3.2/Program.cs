2ed236deabb3e25178086b07a99fdd26b72367a5
PC <PC@LAPTOP-CAK344SD>
2026-08-27 14:56:15 +0700

    Add project files.

---
+++ btap3.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhap b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhap c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        if (a == 0)
+        {
+            // Phuong trinh bac nhat: bx + c = 0
+            if (b == 0)
+            {
+                if (c == 0)
+                    Console.WriteLine("Phuong trinh vo so nghiem.");
+                else
+                    Console.WriteLine("Phuong trinh vo nghiem.");
+            }
+            else
+            {
+                double x = -c / b;
+                Console.WriteLine($"Phuong trinh co nghiem x = {x:F2}");
+            }
+        }
+        else
+        {
+            // Phuong trinh bac hai: ax^2 + bx + c = 0
+            double delta = b * b - 4 * a * c;
+
+            if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                Console.WriteLine($"x1 = {x1:F2}");
+                Console.WriteLine($"x2 = {x2:F2}");
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"Nghiem kep x = {x:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Vo nghiem.");
+            }
+        }
+    }
+}
